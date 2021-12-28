060b0cf3e873aa1fa8f53870d0462c093cf591bf
Mohammad Risad <83496132+Risad212@users.noreply.github.com>
2021-12-28 17:19:11 +0600

    done

--- src/components/tempapp/Tempapp.js
+++ src/components/tempapp/Tempapp.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import Display from '../displayData/Display';
+import NotFound from '../NotFound/NotFound';
 import './Tempapp.css';
 
 // https://api.openweathermap.org/data/2.5/weather?q="+ city + "&units=metric&appid=" + this.apiKey
 
 const Tempapp = () => {
     const [city, setCity] = useState()
-
-    const [search , setSearch] = useState('london')
+    const [search , setSearch] = useState('Dhaka')
 
     function searchBtn(){
       let userValue = document.getElementById('inputCity').value;
@@ -18,9 +18,8 @@ const Tempapp = () => {
      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=ba50a4ec255c0e7036508cb12d91e465`)
       .then(res => res.json())
       .then(data => setCity(data))
-    },[])
+    },[]);
 
-   
     return (
         <div>
            <section className="Tempwrapper">
@@ -29,7 +28,7 @@ const Tempapp = () => {
                <button onClick={searchBtn}>Search</button>
                 {
                   !city ? (
-                    <p>Data not found</p>
+                    <NotFound></NotFound>
                   ): (
                     <Display city={city}></Display>
                   )
