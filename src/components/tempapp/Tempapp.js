a881fd54035f458e057bc81c72f3c509385863b2
Mohammad Risad <83496132+Risad212@users.noreply.github.com>
2021-12-30 18:03:34 +0600

    getting error

--- src/components/tempapp/Tempapp.js
+++ src/components/tempapp/Tempapp.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import Display from '../displayData/Display';
-import NotFound from '../NotFound/NotFound';
 import './Tempapp.css';
 
-// https://api.openweathermap.org/data/2.5/weather?q="+ city + "&units=metric&appid=" + this.apiKey
+//`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=ba50a4ec255c0e7036508cb12d91e465`
+
+// document.body.style.backgroundImage =
+// `url('https://source.unsplash.com/1600x900/? ${city Name}')`
+
 
 const Tempapp = () => {
-    const [city, setCity] = useState()
-    const [search , setSearch] = useState('Dhaka')
+    const [city, setCity] = useState([])
+    const [search , setSearch] = useState('new york')
 
     function searchBtn(){
       let userValue = document.getElementById('inputCity').value;
@@ -15,24 +17,37 @@ const Tempapp = () => {
     }
 
     useEffect(() =>{
-     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=ba50a4ec255c0e7036508cb12d91e465`)
-      .then(res => res.json())
-      .then(data => setCity(data))
+     const fetchApi = async () => {
+       const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=ba50a4ec255c0e7036508cb12d91e465`;
+       const response = await fetch(url)
+       const resJson = await response.json()
+       setCity(resJson)
+     }
+    fetchApi()
     },[]);
-
+    
+    // const {name} = city
+    // const {temp,humidity} = city.main
+    // const {speed} = city.wind
+    // const {icon} = city.weather
+    console.log(city)
     return (
         <div>
            <section className="Tempwrapper">
                <h2>Search City</h2>
                <input type="text" placeholder='search city' id="inputCity" />
                <button onClick={searchBtn}>Search</button>
-                {
-                  !city ? (
-                    <NotFound></NotFound>
-                  ): (
-                    <Display city={city}></Display>
-                  )
-                }
+                {/* display data  */}
+                <section className='weatherDisplay'>
+                  <h3>{name}</h3>
+                  <h4>{`${temp} °C`}</h4>
+                  <div className='wrapper'>
+                    <img src={`https://openweathermap.org/img/wn/56`} alt="" class="icon" />
+                    <h4 class="description">cloud</h4>
+                </div>
+                  <p className='humidity'>{`Humidity ${humidity}%`}</p>
+                    <p className='wind'>{`Wind speed ${speed} km/h`}</p>
+                </section>
            </section>
         </div>
     );
